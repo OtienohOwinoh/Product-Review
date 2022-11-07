61989f85016e56f3a066372266265ab608b483a9
OtienohOwinoh <coowinoh@gmail.com>
2022-11-07 08:38:17 +0300

    Complete Challenge

---
+++ app/review.rb
@@ -0,0 +1,10 @@
+
+class Review < ActiveRecord::Base
+    belongs_tu :user
+    belongs_to :product
+
+    def print_review(user, star_rating, comment)
+        puts "Review for #{self.product.name} by #{self.user.name}: #{self.star_rating}.#{self.comment}"
+    end
+
+end
